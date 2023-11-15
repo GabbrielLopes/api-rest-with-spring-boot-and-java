249811a8e1c8a97221be6fd331f4d258ad6cefc7
GabbrielLps <jhosegabriel95@gmail.com>
2023-11-14 21:40:57 -0300

    feat: code review
    feat: code review

--- 06_ParametersAndExceptionHandler/api-rest-with-spring-boot-and-java/src/main/java/br/com/gabriel/controller/MathController.java
+++ 06_ParametersAndExceptionHandler/api-rest-with-spring-boot-and-java/src/main/java/br/com/gabriel/controller/MathController.java
@@ -1,56 +1,52 @@
 package br.com.gabriel.controller;
 
 import br.com.gabriel.exception.UnsupportedMathOperationException;
-import br.com.gabriel.service.MathServiceImpl;
-import io.micrometer.common.util.StringUtils;
+import br.com.gabriel.service.SimpleMath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
-import static org.apache.tomcat.util.http.parser.HttpParser.isNumeric;
-
 @RestController
 public class MathController {
 
-    @Autowired
-    private MathServiceImpl mathService;
+    SimpleMath simpleMath = new SimpleMath();
 
     @GetMapping("/sum/{numberOne}/{numberTwo}")
     public ResponseEntity<Double> sum(@PathVariable(value = "numberOne") String numberOne,
                                       @PathVariable(value = "numberTwo") String numberTwo) {
-
-        return ResponseEntity.ok(mathService.sum(numberOne,numberTwo));
+        return ResponseEntity.ok(simpleMath.sum(
+                simpleMath.convertToDouble(numberOne), simpleMath.convertToDouble(numberTwo)));
     }
 
     @GetMapping("/subtract/{numberOne}/{numberTwo}")
     public ResponseEntity<Double> subtract(@PathVariable(value = "numberOne") String numberOne,
-                                      @PathVariable(value = "numberTwo") String numberTwo) {
-
-        return ResponseEntity.ok(mathService.subtract(numberOne, numberTwo));
+                                           @PathVariable(value = "numberTwo") String numberTwo) {
+        return ResponseEntity.ok(simpleMath.subtract(simpleMath.convertToDouble(numberOne),
+                simpleMath.convertToDouble(numberTwo)));
     }
 
     @GetMapping("/multiplication/{numberOne}/{numberTwo}")
     public ResponseEntity<Double> multiplication(@PathVariable(value = "numberOne") String numberOne,
-                                      @PathVariable(value = "numberTwo") String numberTwo) {
-
-        return ResponseEntity.ok(mathService.multiplication(numberOne,numberTwo));
+                                                 @PathVariable(value = "numberTwo") String numberTwo) {
+        return ResponseEntity.ok(simpleMath.multiplication(simpleMath.convertToDouble(numberOne),
+                simpleMath.convertToDouble(numberTwo)));
     }
 
     @GetMapping("/division/{numberOne}/{numberTwo}")
     public ResponseEntity<Double> division(@PathVariable(value = "numberOne") String numberOne,
-                                      @PathVariable(value = "numberTwo") String numberTwo) {
-
-        return ResponseEntity.ok(mathService.division(numberOne, numberTwo));
+                                           @PathVariable(value = "numberTwo") String numberTwo) {
+        return ResponseEntity.ok(
+                simpleMath.division(simpleMath.convertToDouble(numberOne), simpleMath.convertToDouble(numberTwo)));
     }
 
     @GetMapping("/squareRoot/{number}")
     public ResponseEntity<Double> squareRoot(@PathVariable(value = "number") String number) {
-
-        return ResponseEntity.ok(mathService.squareRoot(number));
+        if(!simpleMath.isNumeric(number)){
+            throw new UnsupportedMathOperationException("Please set a number valid!");
+        }
+        return ResponseEntity.ok(simpleMath.squareRoot(simpleMath.convertToDouble(number)));
     }
 
 
