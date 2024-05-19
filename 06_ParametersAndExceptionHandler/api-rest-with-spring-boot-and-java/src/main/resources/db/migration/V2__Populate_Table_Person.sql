bfde7d80b97964415cd9b71b1c65daaaab00fe0a
GabbrielLps <jhosegabriel95@gmail.com>
2024-05-19 12:14:49 -0300

    feat: implementa uso de migration flyway
    feat: implementa uso de migration flyway

---
+++ 06_ParametersAndExceptionHandler/api-rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO rest_with_spring_boot_erudio.person (id,address,first_name,gender,last_name) VALUES
+         (1,'São Paulo','Vitin','Male','Santos'),
+         (2,'Address','firstName5','Male','lastName5'),
+         (52,'Address','firstName5','Female','lastName5'),
+         (102,'Teste - South Africa','Nelson','Male','Mandela'),
+         (103,'Teste - South Africa2','Nelson2','Male','Mandela2'),
+         (104,'American','John','Male','Wick');
