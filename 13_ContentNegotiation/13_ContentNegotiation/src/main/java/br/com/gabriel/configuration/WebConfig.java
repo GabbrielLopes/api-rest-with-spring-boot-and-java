cc8e32a6eef29d1ce2174330422c34994232b46e
GabbrielLps <jhosegabriel95@gmail.com>
2024-11-15 00:25:19 -0300

    feat: implementa content negotiation via header param
    feat: implementa content negotiation via header param

--- 13_ContentNegotiation/13_ContentNegotiation/src/main/java/br/com/gabriel/configuration/WebConfig.java
+++ 13_ContentNegotiation/13_ContentNegotiation/src/main/java/br/com/gabriel/configuration/WebConfig.java
@@ -10,10 +10,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-//        http://localhost:8080/person/52?mediaType=xml
-
+//        Via query param http://localhost:8080/person/52?mediaType=xml
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+        */
+
+//        Via HEADER param http://localhost:8080/person/52
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
