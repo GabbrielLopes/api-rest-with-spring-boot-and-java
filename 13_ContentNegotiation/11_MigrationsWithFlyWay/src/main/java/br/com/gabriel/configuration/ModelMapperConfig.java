633d38664e6eb1313a8fc9952b36632d2ec71eaa
GabbrielLps <jhosegabriel95@gmail.com>
2024-11-14 23:55:54 -0300

    feat: copia projeto para sessao 13

---
+++ 13_ContentNegotiation/11_MigrationsWithFlyWay/src/main/java/br/com/gabriel/configuration/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package br.com.gabriel.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Logger;
+
+@Configuration
+public class ModelMapperConfig {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Bean
+    public ModelMapper modelMapper() {
+        logger.info("Iniclizado ModelMapper - API");
+
+        final ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+
+}
---
+++ 13_ContentNegotiation/11_MigrationsWithFlyWay/src/main/java/br/com/gabriel/service/PersonServiceImpl.java
@@ -0,0 +1,95 @@
+package br.com.gabriel.service;
+
+import br.com.gabriel.exception.ResourceNotFoundException;
+import br.com.gabriel.mapper.Mapper;
+import br.com.gabriel.mapper.custom.PersonMapper;
+import br.com.gabriel.model.Person;
+import br.com.gabriel.repository.PersonRepository;
+import br.com.gabriel.service.interfaces.PersonService;
+import br.com.gabriel.vo.v1.PersonVO;
+import br.com.gabriel.vo.v2.PersonVOV2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+
+    @Override
+    public PersonVO findById(Long id) {
+        logger.info("Finding a person...");
+
+        Person personEntity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        return Mapper.parseObject(personEntity, PersonVO.class);
+    }
+
+    @Override
+    public List<PersonVO> findAll() {
+        logger.info("Finding all people!");
+        return Mapper.parseListObjects(repository.findAll(), PersonVO.class);
+    }
+
+    @Override
+    @Transactional
+    public PersonVO create(PersonVO personVO) {
+        logger.info("Creating one personVO!");
+
+        // Realiza conversões de VO para entity / entity para VO
+        Person personEntity = Mapper.parseObject(personVO, Person.class);
+        personVO = Mapper.parseObject(repository.save(personEntity), PersonVO.class);
+
+        return personVO;
+    }
+
+    @Override
+    @Transactional
+    public PersonVOV2 createV2(PersonVOV2 personVO) {
+        logger.info("Creating one personVOV2!");
+
+        // Realiza conversões customizadas
+        Person personEntity = personMapper.convertVoToEntity(personVO);
+        personVO = personMapper.convertEntityToVo(repository.save(personEntity));
+
+        return personVO;
+    }
+
+    @Override
+    public PersonVO update(PersonVO personVO) {
+        logger.info("Updating one personVO!");
+
+        Person personEntity = repository.findById(personVO.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        personEntity.setFirstName(personVO.getFirstName());
+        personEntity.setLastName(personVO.getLastName());
+        personEntity.setAddress(personVO.getAddress());
+        personEntity.setGender(personVO.getGender());
+
+        return Mapper.parseObject(repository.save(personEntity), PersonVO.class);
+    }
+
+    @Override
+    public void delete(long id) {
+        logger.info("Deleting one person!");
+
+        Person personDBResponse = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        repository.delete(personDBResponse);
+    }
+
+}
