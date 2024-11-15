999d27b53344b22093eb7de5b4380ca3a7f5028a
GabbrielLps <jhosegabriel95@gmail.com>
2024-11-15 12:21:34 -0300

    feat: implementa content negotiation media type yaml
    feat: implementa content negotiation media type yaml

--- 13_ContentNegotiation/13_ContentNegotiation/src/main/java/br/com/gabriel/controller/PersonController.java
+++ 13_ContentNegotiation/13_ContentNegotiation/src/main/java/br/com/gabriel/controller/PersonController.java
@@ -1,11 +1,11 @@
 package br.com.gabriel.controller;
 
 import br.com.gabriel.service.interfaces.PersonService;
+import br.com.gabriel.util.MediaType;
 import br.com.gabriel.vo.v1.PersonVO;
 import br.com.gabriel.vo.v2.PersonVOV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,31 +18,56 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "/{id}",
+            produces = {
+                    MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML})
     public ResponseEntity<PersonVO> findById(@PathVariable("id") Long id) {
 
         return ResponseEntity.status(HttpStatus.OK).body(personService.findById(id));
     }
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML})
     public ResponseEntity<List<PersonVO>> findAll() {
 
         return ResponseEntity.status(HttpStatus.OK).body(personService.findAll());
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML})
     public ResponseEntity<PersonVO> create(@RequestBody PersonVO person) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(personService.create(person));
     }
 
-    @PostMapping("/v2")
+    @PostMapping(value = "/v2",
+            consumes = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML})
     public ResponseEntity<PersonVOV2> createV2(@RequestBody PersonVOV2 person) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(personService.createV2(person));
     }
 
-    @PutMapping
+    @PutMapping(
+            consumes = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML},
+            produces = {MediaType.APPLICATION_JSON,
+                    MediaType.APPLICATION_XML,
+                    MediaType.APPLICATION_YML})
     public ResponseEntity<PersonVO> update(@RequestBody PersonVO person) {
 
         return ResponseEntity.status(HttpStatus.OK).body(personService.update(person));
