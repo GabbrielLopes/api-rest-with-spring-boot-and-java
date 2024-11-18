29c962dd48973e5298759966954ace58551aa87a
GabbrielLps <jhosegabriel95@gmail.com>
2024-11-18 20:50:09 -0300

    feat: copia projeto para sessao 15 swagger
    feat: copia projeto para sessao 15 swagger

---
+++ 15_SwaggerOpenAPI/api-rest-with-spring-boot-and-java/src/main/java/br/com/gabriel/mapper/Mapper.java
@@ -0,0 +1,36 @@
+package br.com.gabriel.mapper;
+
+import br.com.gabriel.model.Person;
+import br.com.gabriel.vo.v1.PersonVO;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Mapper {
+
+    private static ModelMapper mapper = new ModelMapper();
+
+    static {
+        mapper.createTypeMap(
+                        Person.class,
+                        PersonVO.class)
+                .addMapping(Person::getId, PersonVO::setKey);
+
+        mapper.createTypeMap(
+                        PersonVO.class,
+                        Person.class)
+                .addMapping(PersonVO::getKey, Person::setId);
+    }
+
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseListObjects(List<O> origins, Class<D> destination) {
+        return origins.stream()
+                .map(origin -> parseObject(origin, destination))
+                .collect(Collectors.toList());
+    }
+
+}
