10a4a70c9a7f53feeb3b9198a425f5c3c3302282
GabbrielLps <jhosegabriel95@gmail.com>
2024-11-20 13:04:06 -0300

    feat: remove migrations no pacote de teste
    feat: remove migrations no pacote de teste

--- 15_SwaggerOpenAPI/api-rest-with-spring-boot-and-java/src/test/resources/db.migration/V1__Create_Table_Person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `person` (
-      `id` bigint NOT NULL,
-      `first_name` varchar(255) NOT NULL,
-      `last_name` varchar(255) NOT NULL,
-      `address` varchar(255) NOT NULL,
-      `gender` varchar(255) NOT NULL,
-      PRIMARY KEY (`id`)
-);
--- 15_SwaggerOpenAPI/api-rest-with-spring-boot-and-java/src/test/resources/db.migration/V2__Populate_Table_Person.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `person` (id,address,first_name,gender,last_name) VALUES
-         (1,'São Paulo','Vitin','Male','Santos'),
-         (2,'Address','firstName5','Male','lastName5'),
-         (52,'Address','firstName5','Female','lastName5'),
-         (102,'Teste - South Africa','Nelson','Male','Mandela'),
-         (103,'Teste - South Africa2','Nelson2','Male','Mandela2'),
-         (104,'American','John','Male','Wick');
